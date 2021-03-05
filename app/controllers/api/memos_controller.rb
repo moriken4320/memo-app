57fae4bfccd6cb47d78ac18ebd6c084061872e1b
moriken4320 <moriken.4320@gmail.com>
2021-03-05 15:45:31 +0900

    APIの実装(メモ投稿機能)

--- app/controllers/api/memos_controller.rb
+++ app/controllers/api/memos_controller.rb
@@ -3,4 +3,17 @@ class Api::MemosController < ApplicationController
     @memos = Memo.order('created_at DESC')
   end
   
+  def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      render :show, status: :created
+    else
+      render json: @memo.errors, status: :unprocessable_entity
+    end
+  end
+ 
+  private
+    def memo_params
+      params.permit(:title, :description)
+    end
 end
